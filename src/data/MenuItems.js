910ac61854ab6ab93d2d23af587969ad04d9cdfc
angeldelrio23 <angeldelrio23@yahoo.es>
2022-01-12 13:16:30 +0100

    Práctica 2 (corrección en MenuItems)

--- src/data/MenuItems.js
+++ src/data/MenuItems.js
@@ -3,19 +3,31 @@ import { PaginaListas } from '../components/PaginaListas';
 import { Perfil } from '../components/Perfil';
 
 export const MenuItems = [
-  { id: 1, path: '/', title: 'Home', component: Home },
   {
     id: 1,
+    path: '/',
+    title: 'Home',
+    component: Home,
+  },
+  {
+    id: 2,
     path: '/listas',
     title: 'Listas de tareas',
     component: PaginaListas,
   },
-  { id: 1, path: '/perfil', title: 'Perfil', component: Perfil },
+  {
+    id: 3,
+    path: '/perfil',
+    title: 'Perfil',
+    component: Perfil,
+  },
 ];
 
 /*
-path
-id
-title
-component
+
+  path
+  id
+  title
+  component
+
 */
